9d653f6960878668c348f7264511c97635e1a305
mdomenjoud <mdomenjoud@octo.com>
2015-02-26 14:21:39 +0100

    Add autoload check

--- tests/CoffeeMachine/CoffeeMachineTest.php
+++ tests/CoffeeMachine/CoffeeMachineTest.php
@@ -4,14 +4,17 @@ namespace CoffeeMachine;
 
 require __DIR__ . '/../../bootstrap.php';
 
-class CoffeeMachineTest extends \PHPUnit_Framework_TestCase {
+class CoffeeMachineTest extends \PHPUnit_Framework_TestCase
+{
 
-    public function test_Dummy(){
-        $this->assertEquals("Chuck Norris", "Chuck "."Norris");
+    public function test_Dummy()
+    {
+        $this->assertEquals("Chuck Norris", "Chuck " . "Norris");
     }
 
-    public function test_Failing(){
-        $this->assertEquals(42, 4+2);
+    public function test_Failing()
+    {
+        $this->assertEquals(42, 4 + 2);
     }
 
     public function test_autoload()
@@ -20,7 +23,5 @@ class CoffeeMachineTest extends \PHPUnit_Framework_TestCase {
 
         $this->assertNotNull($coffeeMachine);
     }
-
-
 }
  
