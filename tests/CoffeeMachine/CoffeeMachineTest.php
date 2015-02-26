e8b27f5c0f5b5d4817cea4f68edf8136d005ca8f
mdomenjoud <mdomenjoud@octo.com>
2015-02-26 13:23:59 +0100

    Project skeleton with example test

---
+++ tests/CoffeeMachine/CoffeeMachineTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CoffeeMachine;
+
+require __DIR__ . '/../../bootstrap.php';
+
+class CoffeeMachineTest extends \PHPUnit_Framework_TestCase {
+
+    public function test_Dummy(){
+        $this->assertEquals("Chuck Norris", "Chuck "."Norris");
+    }
+
+    public function test_Failing(){
+        $this->assertEquals(42, 4+2);
+    }
+
+
+}
+ 
