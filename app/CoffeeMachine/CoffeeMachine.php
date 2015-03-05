c1b567e76d307208af8cb938e0ac1e73d4dc5b45
mdomenjoud <mdomenjoud@octo.com>
2015-03-05 11:50:49 +0100

    First iteration

--- app/CoffeeMachine/CoffeeMachine.php
+++ app/CoffeeMachine/CoffeeMachine.php
@@ -4,4 +4,17 @@ namespace CoffeeMachine;
 
 class CoffeeMachine {
 
+    public static $DrinkTypes = array(
+        "Tea" => "T",
+        "Coffee" => "C",
+        "Chocolate" => "H"
+    );
+
+    public function makeCommand($drinkType = null)
+    {
+        if (isset(self::$DrinkTypes[$drinkType])) {
+            return self::$DrinkTypes[$drinkType] . "::";
+        }
+        return "M:Unknown drink type";
+    }
 }
--- tests/CoffeeMachine/CoffeeMachineTest.php
+++ tests/CoffeeMachine/CoffeeMachineTest.php
@@ -6,18 +6,46 @@ require __DIR__ . '/../../bootstrap.php';
 
 class CoffeeMachineTest extends \PHPUnit_Framework_TestCase {
 
-    public function test_Dummy(){
-        $this->assertEquals("Chuck Norris", "Chuck " . "Norris");
+    /**
+     * @var CoffeeMachine
+     */
+    private $coffeeMachine;
+
+    public function setup(){
+        $this->coffeeMachine = new CoffeeMachine();
     }
 
-    public function test_Failing()    {
-        $this->assertEquals(42, 4 + 2);
+    public function test_makeCommand_ShouldReturnCoffeeCommand(){
+        //When
+        $result = $this->coffeeMachine->makeCommand("Coffee");
+
+        //Then
+        $this->assertEquals("C::", $result);
     }
 
-    public function test_autoload(){
-        $coffeeMachine = new CoffeeMachine();
+    public function test_makeCommand_ShouldReturnTeaCommand(){
+        //When
+        $result = $this->coffeeMachine->makeCommand("Tea");
 
-        $this->assertNotNull($coffeeMachine);
+        //Then
+        $this->assertEquals("T::", $result);
+    }
+
+    public function test_makeCommand_ShouldReturnChocolateCommand(){
+        //When
+        $result = $this->coffeeMachine->makeCommand("Chocolate");
+
+        //Then
+        $this->assertEquals("H::", $result);
+    }
+
+    public function test_makeCommand_ShouldReturnWrongTypeMessage_WhenDrinkTypeIsUnknown(){
+
+        //When
+        $result = $this->coffeeMachine->makeCommand("Pepsi");
+
+        //Then
+        $this->assertEquals("M:Unknown drink type", $result);
     }
 }
  
