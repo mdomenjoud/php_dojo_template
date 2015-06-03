c4d2992c55b43bf74a3a6e798d41f76600d35dda
mdomenjoud <mdomenjoud@octo.com>
2015-06-03 15:47:43 +0200

    Added missing Beverage QUantity Checker

---
+++ app/CoffeeMachine/BeverageQuantityChecker.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: MDO
+ * Date: 03/06/2015
+ * Time: 15:46
+ */
+
+namespace CoffeeMachine;
+
+
+interface BeverageQuantityChecker {
+
+    public function isEmpty($drinkType);
+
+}
